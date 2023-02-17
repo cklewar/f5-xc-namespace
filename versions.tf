b32e3c17dd8a9efd87c06047181a2f9d7ef19810
cklewar <cklewar@t-online.de>
2023-02-17 13:00:10 +0100

    Launch 0.11.19

--- versions.tf
+++ versions.tf
@@ -8,14 +8,13 @@ terraform {
       name = "f5-xc-namespace"
     }
   }
-
+  
   required_providers {
     volterra = {
-      source  = "volterraedge/volterra"
-      version = "= 0.11.16"
+      source = "volterraedge/volterra"
+      version = "= 0.11.19"
     }
-
     local = ">= 2.2.3"
-    null  = ">= 3.1.1"
+    null = ">= 3.1.1"
   }
 }
